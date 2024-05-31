19f65b0512528e87c9c6fcd30c136232a78a66ef
alimmondal <italimbd@gmail.com>
2024-05-31 16:24:58 +0600

    upd

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -54,7 +54,7 @@ const AllProducts = () => {
             </Table.HeadCell>
           </Table.Head>
           {products.map((shoe) => (
-            <Table.Body key={shoe._id} className="divided-y overflow-x-auto">
+            <Table.Body key={shoe._id} className="divided-y ">
               <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                 {/* <Table.Cell> */}
                 {/* {new Date(post.updatedAt).toLocaleDateString()} */}
